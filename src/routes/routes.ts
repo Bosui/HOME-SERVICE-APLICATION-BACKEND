340036c218828a22704e1cf930b1d912c113ed93
Bosui <rbutauskas@gmail.com>
2024-11-18 20:20:25 +0200

    Paverstas i TS veikia

---
+++ src/routes/routes.ts
@@ -0,0 +1,11 @@
+// src/routes/routes.ts
+
+import express, { Request, Response } from 'express';
+
+const router = express.Router();
+
+router.get('/test', (req: Request, res: Response): void => {
+    res.json({ message: 'Test route works!' });
+});
+
+export default router;
